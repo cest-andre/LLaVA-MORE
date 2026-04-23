dd2edda606fc793341ff8ca420a8a97bfaa867d5
cest-andre <ajl321@hotmail.com>
2026-04-23 03:46:13 +0000

    ++Slurm scripts for projector fine-tuning and full visual fine-tuning.  Removed unneeded scripts

--- scripts/llava-more/ewok_proj/full_finetune_slurm.sh
+++ scripts/llava-more/ewok_proj/full_finetune_slurm.sh
@@ -12,9 +12,10 @@ echo "MASTER PORT: ${MASTER_PORT}"
 
 epochs=1
 model_path=meta-llama/Llama-3.1-8B-Instruct # this variable indicate the path of the used language model
-data_train_path=
-train_text_path=
-output_dir=
+images_path= #  root folder of where COCO train2017 images reside (after zip extraction)
+data_train_path= #  path to llava_instruct_80k.json
+vision_tower=openai/clip-vit-large-patch14-336
+mm_projector_path= #  path to mm_projector.bin file
 
 job_name="your/job/name"
 nnodes=1
@@ -22,24 +23,31 @@ echo "job name: $job_name"
 export TOKENIZER_PATH=$model_path
 
 torchrun \
---nnodes=$nnodes --nproc-per-node=8 --rdzv-endpoint=$MASTER_ADDR --rdzv-id=$job_name --rdzv-backend=c10d \
+--nnodes=$nnodes --nproc-per-node=1 --rdzv-endpoint=$MASTER_ADDR --rdzv-id=$job_name --rdzv-backend=c10d \
 ./src/llava/train/train_mem.py \
 --model_name_or_path $model_path \
 --model_architecture llama_3_1 \
 --version llama_3_1 \
 --data_path $data_train_path \
---text_path $train_text_path \
+--image_folder $images_path \
+--vision_tower $vision_tower \
+--pretrain_mm_mlp_adapter $mm_projector_path \
+--mm_projector_type mlp2x_gelu \
+--mm_vision_select_layer -2 \
+--mm_use_im_start_end False \
+--mm_use_im_patch_token False \
+--image_aspect_ratio pad \
 --group_by_modality_length True \
 --bf16 True \
---output_dir $output_dir \
+--output_dir  \
 --num_train_epochs $epochs \
---per_device_train_batch_size 16 \
---per_device_eval_batch_size 4 \
---gradient_accumulation_steps 1 \
+--per_device_train_batch_size 1 \
+--per_device_eval_batch_size 1 \
+--gradient_accumulation_steps 128 \
 --evaluation_strategy no \
 --save_strategy steps \
---save_steps 50000 \
---save_total_limit 1 \
+--save_steps 5000 \
+--save_total_limit 2 \
 --learning_rate 2e-5 \
 --weight_decay 0. \
 --warmup_ratio 0.03 \
@@ -48,6 +56,6 @@ torchrun \
 --tf32 True \
 --model_max_length 2048 \
 --gradient_checkpointing True \
---dataloader_num_workers 4 \
+--dataloader_num_workers 8 \
 --lazy_preprocess True \
 --report_to none
--- scripts/llava-more/ewok_proj/text_finetune.sh
+++ scripts/llava-more/ewok_proj/text_finetune.sh
@@ -19,11 +19,11 @@
 # echo "MASTER PORT: ${MASTER_PORT}"
 
 epochs=1
-model_path=meta-llama/Llama-2-13b-chat-hf # this variable indicate the path of the used language model
+model_path=meta-llama/Llama-3.1-8B-Instruct # this variable indicate the path of the used language model
 # images_path=local/path
 data_train_path=/home/alongon/data/ewok/llava_instruct_80k.json
 train_text_path=/home/alongon/data/ewok/llava_instruct_80k_text.json
-output_dir=/home/alongon/model_weights/ewok/spoof
+output_dir=/home/alongon/model_weights/ewok/llama3.1_8B_instruct_textonly
 # vision_tower=local/path
 # mm_projector_path=local/path
 
@@ -32,21 +32,25 @@ output_dir=/home/alongon/model_weights/ewok/spoof
 # echo "job name: $job_name"
 export TOKENIZER_PATH=$model_path
 
+# CUDA_VISIBLE_DEVICES=4,5,6,7
+
 torchrun \
---nnodes=1 --nproc-per-node=8 \
+--nnodes=1 --nproc-per-node=1 \
 ./src/llava/train/train_mem.py \
 --model_name_or_path $model_path \
---model_architecture llama_2 \
---version llama_2 \
+--model_architecture llama_3_1 \
+--version llama_3_1 \
+--llm_pad_token pad \
 --data_path $data_train_path \
 --text_path $train_text_path \
 --group_by_modality_length True \
 --bf16 True \
+--optim adamw_8bit \
 --output_dir $output_dir \
 --num_train_epochs $epochs \
---per_device_train_batch_size 16 \
+--per_device_train_batch_size 1 \
 --per_device_eval_batch_size 4 \
---gradient_accumulation_steps 1 \
+--gradient_accumulation_steps 128 \
 --evaluation_strategy no \
 --save_strategy steps \
 --save_steps 50000 \
@@ -59,6 +63,6 @@ torchrun \
 --tf32 True \
 --model_max_length 2048 \
 --gradient_checkpointing True \
---dataloader_num_workers 4 \
+--dataloader_num_workers 0 \
 --lazy_preprocess True \
 --report_to none
